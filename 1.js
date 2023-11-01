2a7073137be01137ea4bc1b4a2dd7efdc23568f8
saffonov <38180730+saffonov@users.noreply.github.com>
2023-11-01 21:54:44 +0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/*
+Дан объект numbers.
+Необходимо в консоль вывести все значения объекта, которые больше или равны 3.
+*/
+
+(() => {
+  const numbers = {
+    key1: 12,
+    key2: 2,
+    key3: 4,
+    key4: 1,
+    key5: -244,
+    key6: 0,
+    key7: 7,
+  };
+
+  // Здесь пишем решение, данный комментарий необходимо стереть.
+
+  Object.values(numbers).forEach((element) => {
+    element >= 3 ? console.log(element) : null;
+  });
+})();
